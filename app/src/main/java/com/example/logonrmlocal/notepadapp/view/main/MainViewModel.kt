ebdbcfba77e869d99c0165504f885a03701e80c4
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-29 17:22:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/notepadapp/view/main/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.logonrmlocal.notepadapp.view.main
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.example.logonrmlocal.notepadapp.model.Nota
+import com.example.logonrmlocal.notepadapp.repository.NotaRepository
+
+class MainViewModel: ViewModel(){
+
+    val notaRepository = NotaRepository()
+
+    var notas: MutableLiveData<List<Nota>> = MutableLiveData()
+    val isLoading: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun buscarTodos(){
+
+        notaRepository.buscarTodos(
+                onComplete = {
+                    isLoading.value = false
+                    notas.value = it
+                },
+                onError = {
+                    isLoading.value = false
+                    notas.value = mutableListOf()
+                })
+
+    }
+}
