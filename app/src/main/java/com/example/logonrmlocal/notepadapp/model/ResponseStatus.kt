388d2f01ac0f312735e4d2546aaa6c9fbcfdc3bb
renan nassar <renan_nassar@hotmail.com>
2018-10-04 21:37:10 -0300

    Implementacao do formulario

---
+++ app/src/main/java/com/example/logonrmlocal/notepadapp/model/ResponseStatus.kt
@@ -0,0 +1,6 @@
+package com.example.logonrmlocal.notepadapp.model
+
+data class ResponseStatus(
+        val successo: Boolean,
+        val mensagem: String
+)
--- app/src/main/java/com/example/logonrmlocal/notepadapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/logonrmlocal/notepadapp/view/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.logonrmlocal.notepadapp.view.main
 
+import android.app.Activity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -9,8 +11,10 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.example.logonrmlocal.notepadapp.R
 import com.example.logonrmlocal.notepadapp.model.Nota
+import com.example.logonrmlocal.notepadapp.view.formulario.FormularioActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var mainViewModel: MainViewModel
     private var adapter:MainListAdapter? = null
+    val FORMULARIO_REQUEST_CODE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +40,9 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.buscarTodos()
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val formularioIntent = Intent(this,FormularioActivity::class.java)//dando intenção de mudar de tela
+
+            startActivityForResult(formularioIntent,FORMULARIO_REQUEST_CODE)
         }
     }
 
@@ -70,6 +77,18 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            FORMULARIO_REQUEST_CODE -> {
+                when(resultCode){
+                    Activity.RESULT_OK -> {mainViewModel.buscarTodos()}
+                    Activity.RESULT_CANCELED -> {Toast.makeText(this,"Cancelou",Toast.LENGTH_SHORT).show()}
+                }
+            }
         }
     }
 }
